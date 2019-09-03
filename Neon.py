63aca579bce1025e6c9d4807c76d1dbdbd2bc6f1
Michael1208 <53876489+Michael1208@users.noreply.github.com>
2019-09-03 10:20:03 -0400

    Update Neon.py

--- Neon.py
+++ Neon.py
@@ -12,4 +12,12 @@ async def on_ready():
     print(bot.user.name)
     print(bot.user.id)
 
+@bot.command()
+@commands.cooldown(1, 3, commands.BucketType.user)
+async def ping(ctx):
+    start = time.monotonic()
+    embed = discord.Embed(title="Neon's Ping!", color=0x0084FD)
+    embed.add_field(name="latency", value="{} ms".format(int(ctx.bot.latency*1000)))
+    await ctx.send(embed=embed)
+
 bot.run(TOKEN)
