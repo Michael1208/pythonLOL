93998efc8b7c059aa7eec734c3b4e48763d81b93
Michael1208 <53876489+Michael1208@users.noreply.github.com>
2019-09-03 08:36:45 -0400

    Update Neon.py

--- Neon.py
+++ Neon.py
@@ -1,7 +1,7 @@
 import discord 
+import os
 from discord.ext import commands
 import asyncio
-import os
 
 bot = commands.Bot(command_prefix='*')
 
@@ -9,8 +9,8 @@ bot = commands.Bot(command_prefix='*')
 async def on_ready():
     print('Bot is ready!')
     print(bot.user.name)
-    print(bot.user.id)
-    
+    print(bot.user.id
+ 
 @bot.command()
 @commands.cooldown(1, 3, commands.BucketType.user)
 async def ping(ctx):
@@ -18,5 +18,5 @@ async def ping(ctx):
     embed = discord.Embed(title="HydroBot's Ping!", color=0x0084FD)
     embed.add_field(name="latency", value="{} ms".format(int(ctx.bot.latency*1000)))
     await ctx.send(embed=embed)
-    
-bot.run(os.getenv('Token'))
+          
+bot.run(os.environ['TOKEN'])
