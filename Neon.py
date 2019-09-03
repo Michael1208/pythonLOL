d43bc38a414d3ac29fb1fb0bc930b966c79e39c5
Michael1208 <53876489+Michael1208@users.noreply.github.com>
2019-09-03 10:55:52 -0400

    Update Neon.py

--- Neon.py
+++ Neon.py
@@ -18,7 +18,6 @@ async def on_ready():
     await bot.change_presence(status=discord.Status.online, activity=game)
 
 @bot.command()
-@commands.cooldown(1, 3, commands.BucketType.user)
 async def ping(ctx):
     start = time.monotonic()
     embed = discord.Embed(title="Neon's Ping!", color=0x0084FD)
