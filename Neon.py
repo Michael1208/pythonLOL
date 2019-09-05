687eb1f391102dea22e47c4510d2d92d508d5acf
Michael1208 <53876489+Michael1208@users.noreply.github.com>
2019-09-05 11:36:51 -0400

    Update Neon.py

--- Neon.py
+++ Neon.py
@@ -4,7 +4,6 @@ import asyncio
 from discord.ext.commands import has_permissions
 import time
 import random
-import json
 import os
 from discord.ext import commands, tasks
 from itertools import cycle
