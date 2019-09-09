34a778b407aad30a02ff90df53864e4a934d2ec5
KyleBurnip <38794622+KyleBurnip@users.noreply.github.com>
2019-09-09 18:55:51 +0100

    Update Neon.py

--- Neon.py
+++ Neon.py
@@ -18,7 +18,19 @@ async def on_ready():
     print(bot.user.name)
     print(bot.user.id)
     print('-----------')
-  
+
+	
+@bot.event
+async def on_ready():
+        status = cycle(['n.help',f'{len(bot.guilds)} Servers!', f'{len(bot.users)} Users!'])	
+        change_status.start()	               
+        print("Neon has started!")
+
+@tasks.loop(seconds=15)
+async def change_status():
+  await bot.change_presence(activity=discord.Game(next(status)))
+
+   
 def owner(ctx):
     return ctx.author.id == 349499497774055429 or 505366642230951984
     
