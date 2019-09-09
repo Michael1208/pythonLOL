af4266a69023e70671d19777d9bb121b1cf36bbb
KyleBurnip <38794622+KyleBurnip@users.noreply.github.com>
2019-09-09 16:16:38 +0100

    Update Neon.py

--- Neon.py
+++ Neon.py
@@ -254,8 +254,6 @@ async def _8ball(ctx, *, question):
 async def servers(ctx):
     string = "\n".join([f"Server: {g.name} Users: {len(g.members)}" for g in bot.guilds])
     await ctx.send(f"I'm Currently In These Severs- \n {string}") 
-    else:
-            await ctx.send(f"Developers Only")
 		    
 
                    
