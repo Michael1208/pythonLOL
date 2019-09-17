6934d569984f1e538d167cc1561a5f3ab26ca0ab
KyleBurnip <38794622+KyleBurnip@users.noreply.github.com>
2019-09-17 16:18:42 +0100

    Update Neon.py

--- Neon.py
+++ Neon.py
@@ -22,9 +22,15 @@ async def on_ready():
 	
 @bot.event
 async def on_ready():       
-    await bot.change_presence(status=discord.Status.online, activity=discord.Activity(type=discord.ActivityType.watching, name=("n.help")))
-    print ("Neon Is Ready")
-   
+    async def on_ready():
+        bot.status = cycle(['n.help',f'{len(bot.guilds)} Servers!', f'{len(bot.users)} Users!'])    
+        change_status.start()                   
+        print("Neon has started!")
+
+@tasks.loop(seconds=15)
+async def change_status():
+    await bot.change_presence(status=discord.Status.online, activity=discord.Activity(type=discord.ActivityType.watching, name=(next(bot.status))))
+
 def owner(ctx):
     return ctx.author.id in (349499497774055429, 505366642230951984)
     
@@ -310,22 +316,6 @@ async def say(ctx, *, content):
     else:
         await ctx.send(content)
         await ctx.message.delete()
-
-@bot.command()
-async def status(ctx,status,*,game:str="n.help"):
-    try:
-        await ctx.message.delete()
-    except: pass
-    if status.lower() == "online":
-        await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=game)) 
-    if status.lower() == "idle":
-        await bot.change_presence(status=discord.Status.idle,discord.Activity(type=discord.ActivityType.watching, name=game))
-    if status.lower() == "dnd":
-        await bot.change_presence(status=discord.Status.dnd,activity=discord.Activity(type=discord.ActivityType.watching, name=game))
-    if status.lower() == "invisible":
-        await bot.change_presence(status=discord.Status.invisible,activity=None)
-    if status.lower() == "streaming":
-        await bot.change_presence(activity=discord.Streaming(name=game,url=f"twitch.tv/eltontay11"))
 
 @bot.command()
 async def roll(ctx, dice: str):
