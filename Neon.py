8be3906311477ff85267af1c17a3b6efbd94c20f
Michael1208 <53876489+Michael1208@users.noreply.github.com>
2019-09-17 10:03:11 -0400

    Update Neon.py

--- Neon.py
+++ Neon.py
@@ -318,7 +318,7 @@ async def say(ctx, *, content):
         await ctx.message.delete()
 
 @bot.command()
-@has_permissions(manage_roles=True)
+@has_permissions(administrator=True)
 async def status(ctx,status,*,game:str=None):
     """Sets the bot status.
     status can be "online","idle","dnd","invisible","streaming" while game will be the display game.
